7862fc69df20a401e34d8d5a9c2287d6000e60fc
David Morales <davidm62@usc.edu>
2021-08-25 09:28:08 -0700

    Update hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,16 +1,9 @@
 #include <iostream>
 
-<<<<<<< HEAD
 using namespace std;
 
 int main(){
-    cout << "Hello World" << endl;
+    cout << "Fight on!" << endl;
     return 0;
 }
 
-=======
-int main(){
-    std::cout << "Hello World" << std::endl;
-    return 0;
-}
->>>>>>> e63c4fef71987f0d98b0e37441d972940e7fa804
